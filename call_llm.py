e15f148cb308e18f196568ef4f48fc01d5461946
Jay Lodha <jaylodha@Jays-MacBook-Pro.local>
2024-12-07 10:51:50 -0800

    call_llm function added

---
+++ call_llm.py
@@ -0,0 +1,41 @@
+import os
+from groq import Groq
+from loguru import logger
+import time
+
+
+def call_llm(system_msg: str, model_name: str, get_json: bool= True):
+
+        # url = "https://api.groq.com/openai/v1"
+        key = "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"
+
+        client = Groq(
+            api_key=key,
+        )
+        # client = Groq(api_key=key)
+        logger.debug(f"Model being used: {model_name}")
+        start = time.time()
+        completion = client.chat.completions.create(
+            extra_headers={},
+            model=model_name,
+            response_format={"type": "text" if not get_json else "json_object"},
+            temperature=0,
+            messages=(
+                [
+                    {
+                        "role": "system",
+                        "content": system_msg,
+                    }
+                ]
+                if get_json
+                else [
+                        {
+                            "role": "system",
+                            "content": system_msg,
+                        }
+                ]
+            ),
+        )
+        end = time.time()
+        time_taken = end - start
+        return completion.choices[0].message.content.replace("\n", ""), time_taken
